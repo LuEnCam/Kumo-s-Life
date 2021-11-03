b84280cd55a9045cfa291b6b27c48fd3291fc109
Léon Muller <leon.muller@he-arc.ch>
2021-11-03 20:00:55 +0100

    Added background video on main activity

--- app/src/main/java/ch/hearc/kumoslife/MainActivity.kt
+++ app/src/main/java/ch/hearc/kumoslife/MainActivity.kt
@@ -5,17 +5,40 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import ch.hearc.kumoslife.statistics.StatisticsActivity
+import android.widget.VideoView
+import android.media.MediaPlayer
+
+import android.media.MediaPlayer.OnPreparedListener
+import android.util.Log
 
 class MainActivity : AppCompatActivity()
 {
+	private val resPath: String = "android.resource://ch.hearc.kumoslife/"
+	private lateinit var bgVideoView: VideoView
+
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
+		// Background video
+		bgVideoView = findViewById(R.id.mainBgVideo)
+		bgVideoView.setVideoPath(resPath + R.raw.night)
+		bgVideoView.setOnPreparedListener { mp ->
+			mp.isLooping = true
+			mp.setVolume(0.0F, 0.0F)
+		}
+		bgVideoView.start()
+
+		// Statistics
 		findViewById<Button>(R.id.mainToStatisticsButton).setOnClickListener() {
 			intent = Intent(this, StatisticsActivity::class.java)
 			startActivity(intent)
 		}
+	}
+
+	override fun onResume() {
+		super.onResume()
+		bgVideoView.start()
 	}
 }
