85f7c591daaf2fcc768a020020df99bae28a1d7e
Luca Campana <luca.campana@he-arc.ch>
2021-11-25 09:11:30 +0100

    code cleanup and refactor

--- app/src/main/java/ch/hearc/kumoslife/MainActivity.kt
+++ app/src/main/java/ch/hearc/kumoslife/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.bumptech.glide.Glide
@@ -15,32 +14,29 @@ import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import org.json.JSONObject
 import java.net.URL
-import java.text.SimpleDateFormat
-import java.util.*
 import android.content.Intent
 import ch.hearc.kumoslife.statistics.StatisticsActivity
 import android.widget.VideoView
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import kotlin.time.ExperimentalTime
 
 class MainActivity : AppCompatActivity() {
-	private val LOCATION_PERMISSION_REQ_CODE = 1000;
+	private val LOCATION_PERMISSION_REQ_CODE = 1000
 	private lateinit var fusedLocationClient: FusedLocationProviderClient
 	var latitude: Double = 0.0
 	var longitude: Double = 0.0
+	var actualTime: LocalDateTime = LocalDateTime.MIN
 	private val resPath: String = "android.resource://ch.hearc.kumoslife/"
 	private lateinit var bgVideoView: VideoView
 
 	var place = ""
-	val API = "9d783bddf8b3eaa718e7d926a18ccb1c"
+	val API = "9d783bddf8b3eaa718e7d926a18ccb1c"	//API key used : allows 60 calls per minute
 
+	@ExperimentalTime
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
-
-		// Luca.C - 28.10.2021 : initialize fused location client
-		fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-		findViewById<Button>(R.id.btGetLocation).setOnClickListener {
-			getCurrentLocation()
-		}
 
 		//
 		val cloudSpriteView = findViewById<SpriteView>(R.id.kumo_spriteView)
@@ -57,17 +53,24 @@ class MainActivity : AppCompatActivity() {
 		bgVideoView.setVideoPath(resPath + R.raw.day)
 		bgVideoView.setOnPreparedListener { mp ->
 			mp.isLooping = true
-			mp.setVolume(0.0F, 0.0F)
+			mp.setVolume(0.0F, 0.0F)	//no need of volume
 		}
-		bgVideoView.start()
+		//bgVideoView.start()
 
 		// Statistics
 		findViewById<Button>(R.id.mainToStatisticsButton).setOnClickListener() {
 			intent = Intent(this, StatisticsActivity::class.java)
 			startActivity(intent)
 		}
+
+		// Luca.C - 28.10.2021 : initialize fused location client
+		fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+
+		//Directly fetch localisation at app startup
+		getCurrentLocation()
 	}
 
+	@ExperimentalTime
 	private fun getCurrentLocation() {
 		// checking location permission
 		if (ActivityCompat.checkSelfPermission(
@@ -79,7 +82,7 @@ class MainActivity : AppCompatActivity() {
 			ActivityCompat.requestPermissions(
 				this,
 				arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQ_CODE
-			);
+			)
 			return
 		}
 		fusedLocationClient.lastLocation
@@ -87,6 +90,7 @@ class MainActivity : AppCompatActivity() {
 				// getting the last known or current location
 				latitude = location.latitude
 				longitude = location.longitude
+				actualTime = LocalDateTime.now()
 
 				weatherTask(this).execute()
 
@@ -101,7 +105,8 @@ class MainActivity : AppCompatActivity() {
 
 	inner class weatherTask(_mainActivity: MainActivity) : AsyncTask<String, Void, String>() {
 
-		private val mainActivity = _mainActivity
+		//private val mainActivity = _mainActivity
+
 		override fun doInBackground(vararg params: String?): String? {
 			var response: String?
 			try {
@@ -120,10 +125,11 @@ class MainActivity : AppCompatActivity() {
 			try {
 				/* Extracting JSON returns from the API */
 				val jsonObj = JSONObject(result)
+
+				/*
 				val main = jsonObj.getJSONObject("main")
 				val sys = jsonObj.getJSONObject("sys")
 				val wind = jsonObj.getJSONObject("wind")
-				val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
 
 				val updatedAt: Long = jsonObj.getLong("dt")
 				val updatedAtText =
@@ -140,10 +146,11 @@ class MainActivity : AppCompatActivity() {
 				val sunset: Long = sys.getLong("sunset")
 				val windSpeed = wind.getString("speed")
 				val weatherDescription = weather.getString("description")
+				*/
+
+				val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
 				val weatherID = weather.getString("main")
 
-				findViewById<TextView>(R.id.textView_weater_main).text =
-					"Weather description ${weatherID}"
 
 				/* TO CHECK :
 				- Thunderstorm
@@ -161,26 +168,47 @@ class MainActivity : AppCompatActivity() {
 				- Tornado
 				- Clear
 				- Clouds
-
 				*/
 
+				//Getting the hours of the actual time to change the background
+				val formatter = DateTimeFormatter.ofPattern("HH")
+				val formatted = actualTime.format(formatter)
+
+				var isDay = if (formatted.toInt() in 7..17) true else false // We determine the day time between 7h and 16h
+
+
+				//Depending on the API results, we will use the correct video
 				when (weatherID) {
-					"Fog" -> bgVideoView.setVideoPath(resPath + R.raw.fog)
-					"Rain" -> bgVideoView.setVideoPath(resPath + R.raw.rain)
-					"Snow" -> bgVideoView.setVideoPath(resPath + R.raw.snow_day)
-					else -> { // Note the block
-						bgVideoView.setVideoPath(resPath + R.raw.day)
+					"Fog" ->
+					{
+						if (isDay) bgVideoView.setVideoPath(resPath + R.raw.fog)
+						else bgVideoView.setVideoPath(resPath + R.raw.fog) //TODO : find fog at night video
 					}
+					"Rain" ->
+					{
+						if (isDay) bgVideoView.setVideoPath(resPath + R.raw.rain)
+						else bgVideoView.setVideoPath(resPath + R.raw.rain) //TODO : find rain at night video
+					}
+					"Snow" ->
+					{
+						if (isDay) bgVideoView.setVideoPath(resPath + R.raw.snow_day)
+						else bgVideoView.setVideoPath(resPath + R.raw.snow_night)
+					}
+					else -> {
+						if (isDay) bgVideoView.setVideoPath(resPath + R.raw.day)
+						else bgVideoView.setVideoPath(resPath + R.raw.night)
+					}
+
 				}
 
-				val address = jsonObj.getString("name") + ", " + sys.getString("country")
+				bgVideoView.start()
+
+				//val address = jsonObj.getString("name") + ", " + sys.getString("country")
 
 			} catch (e: Exception) {
 				println("Error !!")
 			}
-
 		}
-
 	}
 
 	override fun onResume() {
