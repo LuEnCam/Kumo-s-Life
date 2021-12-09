a2f39baac8845f180e1be98be35620da7533b7cd
Léon Muller <leon.muller@he-arc.ch>
2021-12-09 10:07:48 +0100

    Refactored and added logs

--- app/src/main/java/ch/hearc/kumoslife/model/AppDatabase.kt
+++ app/src/main/java/ch/hearc/kumoslife/model/AppDatabase.kt
@@ -1,11 +1,13 @@
 package ch.hearc.kumoslife.model
 
 import android.content.Context
+import android.util.Log
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import ch.hearc.kumoslife.model.statistics.Statistic
 import ch.hearc.kumoslife.model.statistics.StatisticDao
+import ch.hearc.kumoslife.views.statistics.StatisticsWorker
 
 @Database(entities = [Statistic::class], version = 1)
 abstract class AppDatabase : RoomDatabase()
@@ -13,6 +15,7 @@ abstract class AppDatabase : RoomDatabase()
     // Static instance: singleton
     companion object
     {
+        private val TAG: String = AppDatabase::class.java.name
         private const val DB_NAME: String = "kumo_db"
         private var instance: AppDatabase? = null;
 
@@ -20,6 +23,7 @@ abstract class AppDatabase : RoomDatabase()
         {
             if (instance == null)
             {
+                Log.i(TAG, "Data base initialization")
                 instance = Room.databaseBuilder(context, AppDatabase::class.java, DB_NAME).build()
             }
             return instance as AppDatabase
--- app/src/main/java/ch/hearc/kumoslife/views/statistics/StatisticsWorker.kt
+++ app/src/main/java/ch/hearc/kumoslife/views/statistics/StatisticsWorker.kt
@@ -9,11 +9,13 @@ import ch.hearc.kumoslife.modelview.StatisticViewModel
 class StatisticsWorker(context: Context, params: WorkerParameters) : Worker(context, params)
 {
     private val viewModel: StatisticViewModel = StatisticViewModel.getInstance()
+    private val TAG: String = StatisticsWorker::class.java.name
 
     override fun doWork(): Result
     {
         return try
         {
+            Log.i(TAG, "progress all statistics")
             viewModel.progressAllStatistics()
             Result.success()
         }
