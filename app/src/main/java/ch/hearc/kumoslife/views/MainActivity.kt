ab2562ff55888e481f79bcb4451a1922ebdac837
Luca Campana <luca.campana@he-arc.ch>
2021-12-09 09:36:47 +0100

    replaced async tasks with executors & handlers

--- app/src/main/java/ch/hearc/kumoslife/views/MainActivity.kt
+++ app/src/main/java/ch/hearc/kumoslife/views/MainActivity.kt
@@ -7,6 +7,9 @@ import android.location.Location
 import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.Toast
@@ -30,6 +33,7 @@ import kotlin.time.ExperimentalTime
 import ch.hearc.kumoslife.views.shop.ShopActivity
 import com.google.android.gms.location.LocationCallback
 import com.google.android.gms.location.LocationResult
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity()
 {
@@ -42,6 +46,10 @@ class MainActivity : AppCompatActivity()
     var actualTime: LocalDateTime = LocalDateTime.MIN
     var place = ""
     val API = "9d783bddf8b3eaa718e7d926a18ccb1c"    //API key used : allows 60 calls per minute
+
+    //Use of Executor and handler to replace AsyncTasks
+    val myExecutor = Executors.newSingleThreadExecutor()
+    val myHandler = Handler(Looper.getMainLooper())
 
     private lateinit var bgVideoView: VideoView
     private lateinit var viewModel: StatisticViewModel
@@ -101,7 +109,17 @@ class MainActivity : AppCompatActivity()
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
         //Directly fetch localisation at app startup
-        getCurrentLocation()
+        val lightTimerTask: TimerTask = object : TimerTask()
+        {
+            override fun run()
+            {
+                getCurrentLocation()
+                Log.i("Timer","Launching location & weatherTask")
+            }
+        }
+        val WeatherTimer = Timer()
+        WeatherTimer.scheduleAtFixedRate(lightTimerTask,0,1000*60*1)
+
     }
 
     override fun onResume()
@@ -153,14 +171,15 @@ class MainActivity : AppCompatActivity()
                 latitude = location.latitude
                 longitude = location.longitude
                 actualTime = LocalDateTime.now()
-                weatherTask(this).execute()
+                launchExecutor()
             }
             else if (newLocation !== null)
             {
                 latitude = newLocation!!.latitude
                 longitude = newLocation!!.longitude
                 actualTime = LocalDateTime.now()
-                weatherTask(this).execute()
+                launchExecutor()
+
             }
             else
             {
@@ -170,116 +189,118 @@ class MainActivity : AppCompatActivity()
         }.addOnFailureListener { Toast.makeText(this, "Failed on getting current location", Toast.LENGTH_SHORT).show() }
     }
 
-    inner class weatherTask(_mainActivity: MainActivity) : AsyncTask<String, Void, String>()
+    private fun launchExecutor()
     {
-        //private val mainActivity = _mainActivity
+        myExecutor.execute {
+            val response = weatherTaskExecution()
+            myHandler.post {
+                checkWeatherResponse(response)
+            }
+        }
+    }
 
-        override fun doInBackground(vararg params: String?): String?
+    private fun weatherTaskExecution() : String?
+    {
+        var response: String?
+        try
         {
-            var response: String?
-            try
-            {
-                response = URL("https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$API").readText(Charsets.UTF_8)
-            }
-            catch (e: Exception)
-            {
-                response = null
-            }
-            return response
+            response = URL("https://api.openweathermap.org/data/2.5/weather?lat=$latitude&lon=$longitude&appid=$API").readText(Charsets.UTF_8)
+        }
+        catch (e: Exception)
+        {
+            response = null
         }
 
-        override fun onPostExecute(result: String?)
+        return response
+    }
+
+    private fun checkWeatherResponse(result : String?)
+    {
+        try
         {
-            super.onPostExecute(result)
-            try
+            /* Extracting JSON returns from the API */
+            val jsonObj = JSONObject(result)
+
+            /*
+            val main = jsonObj.getJSONObject("main")
+            val sys = jsonObj.getJSONObject("sys")
+            val wind = jsonObj.getJSONObject("wind")
+
+            val updatedAt: Long = jsonObj.getLong("dt")
+            val updatedAtText =
+                "Updated at: " + SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(
+                    Date(updatedAt * 1000)
+                )
+            val temp = main.getString("temp") + "°C"
+            val tempMin = "Min Temp: " + main.getString("temp_min") + "°C"
+            val tempMax = "Max Temp: " + main.getString("temp_max") + "°C"
+            val pressure = main.getString("pressure")
+            val humidity = main.getString("humidity")
+
+            val sunrise: Long = sys.getLong("sunrise")
+            val sunset: Long = sys.getLong("sunset")
+            val windSpeed = wind.getString("speed")
+            val weatherDescription = weather.getString("description")
+            */
+
+            val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
+            val weatherID = weather.getString("main")
+
+
+            /* TO CHECK :
+            - Thunderstorm
+            - Drizzle
+            - Rain
+            - Snow
+            - Mist
+            - Smoke
+            - Haze
+            - Dust
+            - Ash
+            - Fog
+            - Sand
+            - Squall
+            - Tornado
+            - Clear
+            - Clouds
+            */
+
+            //Getting the hours of the actual time to change the background
+            val formatter = DateTimeFormatter.ofPattern("HH")
+            val formatted = actualTime.format(formatter)
+
+            var isDay = if (formatted.toInt() in 7..17) true else false // We determine the day time between 7h and 16h
+
+
+            //Depending on the API results, we will use the correct video
+            when (weatherID)
             {
-                /* Extracting JSON returns from the API */
-                val jsonObj = JSONObject(result)
-
-                /*
-                val main = jsonObj.getJSONObject("main")
-                val sys = jsonObj.getJSONObject("sys")
-                val wind = jsonObj.getJSONObject("wind")
-
-                val updatedAt: Long = jsonObj.getLong("dt")
-                val updatedAtText =
-                    "Updated at: " + SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(
-                        Date(updatedAt * 1000)
-                    )
-                val temp = main.getString("temp") + "°C"
-                val tempMin = "Min Temp: " + main.getString("temp_min") + "°C"
-                val tempMax = "Max Temp: " + main.getString("temp_max") + "°C"
-                val pressure = main.getString("pressure")
-                val humidity = main.getString("humidity")
-
-                val sunrise: Long = sys.getLong("sunrise")
-                val sunset: Long = sys.getLong("sunset")
-                val windSpeed = wind.getString("speed")
-                val weatherDescription = weather.getString("description")
-                */
-
-                val weather = jsonObj.getJSONArray("weather").getJSONObject(0)
-                val weatherID = weather.getString("main")
-
-
-                /* TO CHECK :
-                - Thunderstorm
-                - Drizzle
-                - Rain
-                - Snow
-                - Mist
-                - Smoke
-                - Haze
-                - Dust
-                - Ash
-                - Fog
-                - Sand
-                - Squall
-                - Tornado
-                - Clear
-                - Clouds
-                */
-
-                //Getting the hours of the actual time to change the background
-                val formatter = DateTimeFormatter.ofPattern("HH")
-                val formatted = actualTime.format(formatter)
-
-                var isDay = if (formatted.toInt() in 7..17) true else false // We determine the day time between 7h and 16h
-
-
-                //Depending on the API results, we will use the correct video
-                when (weatherID)
+                "Fog"  ->
                 {
-                    "Fog"  ->
-                    {
-                        if (isDay) bgVideoView.setVideoPath(resPath + R.raw.fog)
-                        else bgVideoView.setVideoPath(resPath + R.raw.fog) //TODO : find fog at night video
-                    }
-                    "Rain" ->
-                    {
-                        if (isDay) bgVideoView.setVideoPath(resPath + R.raw.rain)
-                        else bgVideoView.setVideoPath(resPath + R.raw.rain) //TODO : find rain at night video
-                    }
-                    "Snow" ->
-                    {
-                        if (isDay) bgVideoView.setVideoPath(resPath + R.raw.snow_day)
-                        else bgVideoView.setVideoPath(resPath + R.raw.snow_night)
-                    }
-                    else   ->
-                    {
-                        if (isDay) bgVideoView.setVideoPath(resPath + R.raw.day)
-                        else bgVideoView.setVideoPath(resPath + R.raw.night)
-                    }
-
+                    if (isDay) bgVideoView.setVideoPath(resPath + R.raw.fog)
+                    else bgVideoView.setVideoPath(resPath + R.raw.fog) //TODO : find fog at night video
                 }
-
-                bgVideoView.start()
-
+                "Rain" ->
+                {
+                    if (isDay) bgVideoView.setVideoPath(resPath + R.raw.rain)
+                    else bgVideoView.setVideoPath(resPath + R.raw.rain) //TODO : find rain at night video
+                }
+                "Snow" ->
+                {
+                    if (isDay) bgVideoView.setVideoPath(resPath + R.raw.snow_day)
+                    else bgVideoView.setVideoPath(resPath + R.raw.snow_night)
+                }
+                else   ->
+                {
+                    if (isDay) bgVideoView.setVideoPath(resPath + R.raw.day)
+                    else bgVideoView.setVideoPath(resPath + R.raw.night)
+                }
             }
-            catch (e: Exception)
-            {
-                println("Error !!")
-            }
+            bgVideoView.start()
+        }
+        catch (e: Exception)
+        {
+            println("Error !!")
         }
     }
 }
